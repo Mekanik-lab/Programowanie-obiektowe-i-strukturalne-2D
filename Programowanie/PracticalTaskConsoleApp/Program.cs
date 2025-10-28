2c0048f7bbfe53a35c897cb5cf2c90c3fd938682
Sala213 <Sala213@ezse.pl>
2025-10-28 14:21:40 +0100

    zr

--- Programowanie/PracticalTaskConsoleApp/Program.cs
+++ Programowanie/PracticalTaskConsoleApp/Program.cs
@@ -3,7 +3,7 @@ using PracticalTaskConsoleApp.Czerwiec2022_2;
 using PracticalTaskConsoleApp.Czerwiec2023_6;
 using PracticalTaskConsoleApp.Styczeń2023_1;
 
-/*Task1 task1 = new Task1();
+Task1 task1 = new Task1();
 task1.Task();
 
 Console.WriteLine("\n");
@@ -20,4 +20,4 @@ Task4 task4 = new Task4();
 task4.Task();
 
 Task5 task5 = new Task5();
-task5.Task();*/
+task5.Task();
